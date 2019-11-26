2f475cbd4fa61b43203c1c064eed2ed36d0cd441
potaza103 <38246136+potaza103@users.noreply.github.com>
2019-11-27 02:06:58 +0700

    27/11/2562

--- app/src/main/java/com/example/projectfitness/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/projectfitness/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.projectfitness.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,8 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import com.example.projectfitness.FitnessActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    ImageView fitness;
 
     private RecyclerView recyclerView;
     private PostAdapter postAdapter;
@@ -63,6 +68,15 @@ public class HomeFragment extends Fragment {
         recyclerView_story.setAdapter(storyAdapter);
 
         progress_circular = view.findViewById(R.id.progress_circular);
+
+        fitness = view.findViewById(R.id.fitness);
+
+        fitness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), FitnessActivity.class));
+            }
+        });
 
         checkFollowing();
 
